eabc88c21a0a52f02c436b530ffb8a1600c9283c
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-06-13 10:38:04 -0300

    footer component

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2022
+            <a href="http://MyBeautifulWebSite.com" target='_blank'> Amanda</a>.
+        </strong>
+    </footer>
+)
