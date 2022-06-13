a0f7d04eaf8ab10035b87bee53552538568415fe
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-06-13 11:21:56 -0300

    grid component

--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -3,8 +3,7 @@ import React from "react";
 export default props => (
     <footer className="main-footer">
         <strong>
-            Copyright &copy; 2022
-            <a href="http://MyBeautifulWebSite.com" target='_blank'> Amanda</a>.
+            Amanda &copy; 2022.
         </strong>
     </footer>
 )
