6dea9d9279cb6e9c5081fc1de8d6a38d65de1c4f
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-06-13 08:59:21 -0300

    index and app component

---
+++ frontend/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'babel-plugin-react-html-attrs'
+import  ReactDOM from 'babel-plugin-react-html-attrs'
+import App from './main/app'
+
+ReactDOM.render(<App/>, document.getElementById('app'))
---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,7 @@
+import React from "babel-plugin-react-html-attrs"
+
+export default props => (
+    <div className="wrapper">
+        <h1>App</h1>
+    </div>
+)
