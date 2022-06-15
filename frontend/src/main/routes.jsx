5f2fee7a0517a5e19c93f9b9227c1a08eeaeffec
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-06-15 12:54:19 -0300

    finished

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Router, Route, Redirect, hashHistory } from "react-router";
 
+import App from "./app";
 import Dashboard2 from "../dashboard2/dashboard2";
 import BillingCycle from "../billingCycle/billingCycle";
 
-export default props => (
-        <Router history={hashHistory}>
-            <Route path='/' component={Dashboard2} />
-            <Route path='/billingCycles' component={BillingCycle} />
-            <Redirect from='*' to='/'/>
-        </Router>
-)
+export default (props) => (
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Dashboard} />
+    </Route>
+    <Route path="/billingCycles" component={BillingCycle} />
+    <Redirect from="*" to="/" />
+  </Router>
+);
