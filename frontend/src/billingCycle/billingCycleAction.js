a8324b46992f7e0898689837a5ef1412766891be
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-06-15 08:40:00 -0300

    include billingcycle

--- frontend/src/billingCycle/billingCycleAction.js
+++ frontend/src/billingCycle/billingCycleAction.js
@@ -1,24 +1,33 @@
 import axios from "axios";
 import { toastr } from "react-redux-toastr";
-const BASE_URL = 'http://localhost:3003/api'
+import { reset as resetForm } from "redux-form";
+import { showTabs, selectTab } from "../common/tab/tabActions";
+
+const BASE_URL = "http://localhost:3003/api";
 
 export function getList() {
-    const request = axios.get(`${BASE_URL}/billingCycles`)
-    return {
-        type: 'BILLING_CYCLES_FETCHED',
-        payload: request
-    }
+  const request = axios.get(`${BASE_URL}/billingCycles`);
+  return {
+    type: "BILLING_CYCLES_FETCHED",
+    payload: request,
+  };
 }
 
 export function create(values) {
-    axios.get(`${BASE_URL}/billingCycles`, values)
-        .then(resp => {
-            toastr.success('Sucesso', 'Operação Realizada com Sucesso.')
-        })
-        .catch(e => {
-            e.response.data.errors.forEach(error => toastr.error('Erro', error))
-        })
-    return {
-        type: 'TEMP'
-    }
+  return (dispatch) => {
+    axios
+      .get(`${BASE_URL}/billingCycles`, values)
+      .then((resp) => {
+        toastr.success("Sucesso", "Operação Realizada com Sucesso.");
+        dispatch([
+            resetForm('billingCycleForm'),
+            getList(),
+            selectTab('tabList'),
+            showTabs('tabList', 'tabCreate')
+        ])
+      })
+      .catch((e) => {
+        e.response.data.errors.forEach((error) => toastr.error("Erro", error));
+      });
+  };
 }
